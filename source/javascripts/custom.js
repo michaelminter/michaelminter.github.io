54bceb1104e91c52d65781c6b9e128267aab4395
Michael Minter <mothore@gmail.com>
2012-12-04 14:48:31 -0600

    update copy

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -65,14 +65,14 @@ $(function(){
     }
   });
 
-  var githubInfo = $("#gh_repos");
-
-  github.showRepos({
-    user: githubInfo.data('github-user'),
-    count: githubInfo.data('github-repo-count'),
-    skip_forks: githubInfo.data('github-skip-forks'),
-    target: githubInfo 
-  });
+  // var githubInfo = $("#gh_repos");
+// 
+  // github.showRepos({
+    // user: githubInfo.data('github-user'),
+    // count: githubInfo.data('github-repo-count'),
+    // skip_forks: githubInfo.data('github-skip-forks'),
+    // target: githubInfo 
+  // });
 
   var $container = $('#post-container');
   $container.imagesLoaded(function(){
